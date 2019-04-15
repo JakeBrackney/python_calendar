f8b0d8c720773efbb2f1b2338c89c7d0e4738b18
Lindsay Urchyk <urchykli@gmail.com>
2019-04-15 11:25:44 -0400

    login screen works, removed accounts app and put everything in calendar app

--- calendar_app/urls.py
+++ calendar_app/urls.py
@@ -3,4 +3,16 @@ from . import views
 
 urlpatterns = [
     path('event/<int:pk>', views.event_detail, name='event_detail'),
+    path('', views.index, name= 'index')
 ]
+
+
+# urlpatterns = [
+#      # Login / Log Out
+#      path('accounts/login/', auth_views.LoginView,
+#           {'template_name': 'accounts/login.html'}, name='login'),
+#      path('accounts/logout/', auth_views.LogoutView,
+#           {'template_name': 'accounts/logged_out.html'}, name='logout'),
+#           # Sign Up
+#      path('accounts/signup', views.sign_up, name="signup")
+# ]
