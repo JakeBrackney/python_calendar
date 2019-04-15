8386c765566f91ae7afd1717eb95e4821a16c4a4
Jake Brackney <brackney.jake@gmail.com>
2019-04-15 10:22:39 -0400

    Some auth debugging, not complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,11 +3,11 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    # Login / Log Out
-    path('accounts/login/', auth_views.login,
-         {'template_name': 'accounts/login.html'}, name='login'),
-    path('accounts/logout/', auth_views.logout,
-         {'template_name': 'accounts/logged_out.html'}, name='logout'),
-         # Sign Up
-     path('accounts/signup', views.sign_up, name="signup"),
+     # Login / Log Out
+     path('accounts/login/', auth_views.LoginView,
+          {'template_name': 'accounts/login.html'}, name='login'),
+     path('accounts/logout/', auth_views.LogoutView,
+          {'template_name': 'accounts/logged_out.html'}, name='logout'),
+          # Sign Up
+     path('accounts/signup', views.sign_up, name="signup")
 ]
