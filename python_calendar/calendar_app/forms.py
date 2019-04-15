18eccb3652a29b3d6b592be0ebf74c104fc3aaaf
Lindsay Urchyk <urchykli@gmail.com>
2019-04-15 17:16:41 -0400

    fixed date and time format

--- python_calendar/calendar_app/forms.py
+++ python_calendar/calendar_app/forms.py
@@ -5,4 +5,24 @@ class EventForm(forms.ModelForm):
 
     class Meta:
         model = Event
-        fields = ('title', 'startDate', 'endDate', 'description', 'location')
+        fields = ('title', 'startDate', 'startTime', 'endDate', 'endTime', 'description', 'location')
+
+    startDate = forms.DateField(
+    widget=forms.DateInput(format='%m/%d/%Y'),
+    input_formats=('%m/%d/%Y', )
+    )
+
+    startTime = forms.TimeField(
+    widget=forms.TimeInput(format='%I:%M %p'),
+    input_formats=('%I:%M %p', )
+    )
+
+    endDate = forms.DateField(
+    widget=forms.DateInput(format='%m/%d/%Y'),
+    input_formats=('%m/%d/%Y', )
+    )
+
+    endTime = forms.TimeField(
+    widget=forms.TimeInput(format='%I:%M %p'),
+    input_formats=('%I:%M %p', )
+    )
