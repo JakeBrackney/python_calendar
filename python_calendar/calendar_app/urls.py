dfc5b62b06c5cb4dd97ca0c2b314ca56e2aa4310
Jake Brackney <brackney.jake@gmail.com>
2019-04-15 16:31:58 -0400

    Event Delete Works-ish

--- python_calendar/calendar_app/urls.py
+++ python_calendar/calendar_app/urls.py
@@ -7,15 +7,5 @@ urlpatterns = [
     path('event_form', views.event_create, name='event_form'),
     path('event/<int:pk>', views.event_detail, name='event_detail'),
     path('event/<int:pk>/event_edit', views.event_edit, name='event_edit'),
+    path('event/<int:pk>/event_delete', views.event_delete, name='event_delete')
 ]
-
-
-# urlpatterns = [
-#      # Login / Log Out
-#      path('accounts/login/', auth_views.LoginView,
-#           {'template_name': 'accounts/login.html'}, name='login'),
-#      path('accounts/logout/', auth_views.LogoutView,
-#           {'template_name': 'accounts/logged_out.html'}, name='logout'),
-#           # Sign Up
-#      path('accounts/signup', views.sign_up, name="signup")
-# ]
