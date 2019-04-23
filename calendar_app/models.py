fc4ce2a2847170dda9998e6ac13b62d7770161e8
Jake Brackney <brackney.jake@gmail.com>
2019-04-23 12:04:51 -0400

    Default date and time populate, wrong timezone

--- calendar_app/models.py
+++ calendar_app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from datetime import datetime
+from datetime import datetime, date
+from django.utils.timezone import now
 # Create your models here.
 
 class Event(models.Model):
 	title = models.CharField(max_length=100)
-	startDate = models.DateField(default=datetime.now)
-	startTime = models.TimeField(default=datetime.now)
+	startDate = models.DateField(("Date"), default=date.today)
+	startTime = models.TimeField(default=now)
 	endDate = models.DateField(blank=True)
 	endTime = models.TimeField(blank=True)
 	# startTime = models.TimeField()
@@ -15,3 +16,7 @@ class Event(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	# def __init__(self, *args, **kwargs):
+	# 	super(EventForm, self).__init__(*args, **kwargs)
+  #   self.fields['trans_date'].initial
