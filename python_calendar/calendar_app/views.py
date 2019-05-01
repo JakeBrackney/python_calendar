c7ac517a06485150a2064b341f7144edc01516fd
Jake Brackney <brackney.jake@gmail.com>
2019-05-01 14:24:20 -0400

    Added prev_month and next_month to views and templates

--- python_calendar/calendar_app/views.py
+++ python_calendar/calendar_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from datetime import datetime
+import calendar
 from django.http import HttpResponse
 from django.views import generic
 from django.utils.safestring import mark_safe
@@ -11,6 +12,8 @@ from .models import Event
 from .utils import Calendar
 from django.contrib.auth.decorators import login_required
 from .forms import EventForm
+from datetime import datetime, timedelta
+from calendar import HTMLCalendar
 
 # from https://www.huiwenteo.com/normal/2018/07/24/django-calendar.html
 class CalendarView(generic.ListView):
@@ -19,14 +22,10 @@ class CalendarView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        # use today's date for the calendar
         d = get_date(self.request.GET.get('day', None))
-
-        # Instantiate our calendar class with today's year and date
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
         cal = Calendar(d.year, d.month)
-
-        # Call the formatmonth method, which returns our calendar as a table
         html_cal = cal.formatmonth(withyear=True)
         context['calendar'] = mark_safe(html_cal)
         return context
@@ -37,6 +36,19 @@ def get_date(req_day):
         year, month = (int(x) for x in req_day.split('-'))
         return date(year, month, day=0)
     return datetime.today()
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
 
 def index(request):
     return render(request, 'calendar_app/base.html')
