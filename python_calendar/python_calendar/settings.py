56f943ef5861a60f20cf2a82d127a3e7d2157826
Lindsay Urchyk <urchykli@gmail.com>
2019-05-23 14:23:41 -0400

    updated static root path

--- python_calendar/python_calendar/settings.py
+++ python_calendar/python_calendar/settings.py
@@ -140,6 +140,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = "/lindsayurchyk/wdi/sideProjects/python_calendar/python_calendar/static_root"
+STATIC_ROOT = "/python_calendar/python_calendar/static_root"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
