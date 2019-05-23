d4b968e914475dab2edd9358182324f867356518
Lindsay Urchyk <urchykli@gmail.com>
2019-05-23 16:59:44 -0400

    changed static root and static url

--- python_calendar/settings.py
+++ python_calendar/settings.py
@@ -135,18 +135,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/calendar_app/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 django_heroku.settings(locals())
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
